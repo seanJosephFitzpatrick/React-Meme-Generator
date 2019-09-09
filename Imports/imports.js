cea837e811846cfb3b05814178cb91fda3c8760d
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2019-09-09 16:55:36 +0100

    Add imports file

---
+++ Imports/imports.js
@@ -0,0 +1,5 @@
+import Header from "../Component/Header";
+import MemeGenerator from "../Component/MemeGenerator";
+import Styles from "../src/styles.css";
+
+export { Header, MemeGenerator, Styles };
--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { Header, MemeGenerator } from "../Imports/imports";
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Header />
+        <MemeGenerator />
+      </div>
+    );
   }
 }
 
