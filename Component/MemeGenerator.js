7e0fcb40e91660f8d496ba7c4b96b723600eafac
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2019-09-09 17:07:03 +0100

    Make call to star wars api

--- Component/MemeGenerator.js
+++ Component/MemeGenerator.js
@@ -6,8 +6,20 @@ class MemeGenerator extends Component {
     this.state = {
       topText: "",
       bottomText: "",
-      randomImg: "http://i.imgflip.com/1bij.jpg"
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImgs: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data;
+        this.setState({
+          allMemeImgs: memes
+        });
+      });
   }
 
   render() {
